c514f1f471762e6ee66716e9725653fb3cdeeade
Joy <daheecha0612@gmail.com>
2020-03-13 15:26:57 +0900

    Initial commit (Set project configuration & Add admin template)

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HelloController{
+    @RequestMapping("/hello")
+    public String hello(String name, Model model){
+        model.addAttribute("name", name);
+        return "oraclejavacommunity"; //name param in view
+    }
+}
